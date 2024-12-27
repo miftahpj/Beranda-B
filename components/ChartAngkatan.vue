dffe63274672499b665597120169cefa0cb43897
miftahpj <142276212+miftahpj@users.noreply.github.com>
2024-12-27 14:22:21 +0700

    Add files via upload

---
+++ components/ChartAngkatan.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Chart from 'chart.js/auto';
+
+const labels = [
+    'Angkatan 10',
+    'Angkatan 11',
+    'Angkatan 12',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Jumlah',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [540, 540, 540],
+    }]
+  };
+
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+  };
+
+onMounted(() => {
+    const myChart = new Chart(
+    document.getElementById('Chart'),
+    config
+  );
+})
+</script>
+
+<template>
+    <div class="pt-5">
+        <canvas id="Chart"></canvas>
+      </div>
+      
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="head">
+        <div class="title">
+            <img src="/assets/logo-smkn4.png" alt="logo">
+            <h2>SMKN Tasikmalaya</h2>
+        </div>
+        <div class="info">
+            <span>Lokasi</span>Purbaratu, Tasikmalaya
+        </div>
+    </div>
+    <ul class="nav justify-content-center sticky-top bg-white bg-opacity-75">
+        <nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold" href="/">Beranda</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Profil
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/profil/sejarah">Sejarah</a></li>
+            <li><a class="dropdown-item" href="/profil/vm">Visi Misi</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/profil/ds">Data Statistic</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Estrakurikuler
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/eskul/ol">Olahraga</a></li>
+            <li><a class="dropdown-item" href="/eskul/nl">Non Olahraga</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Gallery
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/kurikulum/jw">Pembelajaran</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">jadwal</a></li>
+          </ul>
+        </li>
+        <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Siswa
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Sejarah</a></li>
+            <li><a class="dropdown-item" href="#">Visi Misi</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li> -->
+        <!-- <li class="nav-item">
+          <a class="nav-link" href="/artikel">artikel</a>
+        </li> -->
+      </ul>
+      
+
+    </div>
+  </div>
+</nav>
+    </ul>
+    
+<div class="garis"></div>
+</template>
+<style>
+.garis{
+    border-bottom: 1px solid #ccc;
+}
+.head{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    padding: 20px 20px;
+    border-bottom: 1px solid #ccc;
+}
+.title{
+    display: flex;
+    align-items: center;
+
+}
+.title h2{
+    font-size: 29px;
+}
+.title img{
+    height: 60px;
+}
+.info{
+    font-size: 24px;
+    color: #8d8585;
+}
+.info span{
+    font-size: 20px;
+    display: block;
+    color: black;
+}
+</style>
